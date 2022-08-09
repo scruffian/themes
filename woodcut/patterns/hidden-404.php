ca52718174b7166b0d71720f86cd4429047677f4
Ben Dwyer <ben@scruffian.com>
2022-08-09 14:38:24 +0100

    Add woodcut theme

---
+++ woodcut/patterns/hidden-404.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Title: 404 page
+ * Slug: woodcut/hidden-404
+ * Inserter: no
+ */
+?>
+
+<!-- wp:heading {"textAlign":"center","level":1} -->
+<h1 class="has-text-align-center"><?php echo esc_html__( "Oops! That page can&rsquo;t be found.", "woodcut" ); ?></h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p><?php echo esc_html__( "It looks like nothing was found at this location. Maybe try a search?", "woodcut" ); ?></p>
+<!-- /wp:paragraph -->
