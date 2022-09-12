d53c82333bd4545f30c6ac89bfd1e1992b98694b
Ben Dwyer <ben@scruffian.com>
2022-09-12 15:47:49 +0100

    add some i18n text

---
+++ console/patterns/hidden-404.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Title: 404 page
+ * Slug: console/hidden-404
+ * Inserter: no
+ */
+?>
+
+<!-- wp:heading {"level":1} -->
+<h1><?php echo esc_html__( "That page can&rsquo;t be found.", "console" ); ?></h1>
+<!-- /wp:heading -->
